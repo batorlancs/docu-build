0b2db79e10f054dd6f1c869ecd93870048882452
Gergely Bator <gergelybator@pop-os.localdomain>
2023-09-23 17:24:35 +0200

    remove project error not handled bug fixed

--- src/main/home/removeproject.ts
+++ src/main/home/removeproject.ts
@@ -18,8 +18,26 @@ async function removeProjectData(id: string): Promise<void> {
 }
 
 /**
+ * removes a folder and all of its contents
+ * @param path path of the folder to remove
+ * @returns a promise that resolves when the folder is removed
+ */
+const removeFolderAndContents = async (path: string): Promise<void> => {
+    return new Promise((resolve, reject) => {
+        fs.rm(path, { recursive: true }, (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
+};
+
+/**
  * removes a project from the projects folder
  * @param id id of the project to remove
+ * @returns a promise that resolves when the project is removed
  */
 export const removeProject = async (id: string): Promise<void> => {
     // get project path
@@ -29,24 +47,50 @@ export const removeProject = async (id: string): Promise<void> => {
         throw new Error("project does not exist");
     }
     // remove folder at path
-    fs.rm(project.path, { recursive: true }, async (err1) => {
-        try {
-            await removeProjectData(id);
-            if (err1) {
-                throw new Error(`Error removing folder ${err1.message}`);
-            }
-        } catch (err2) {
-            if (err1) {
-                throw new Error(
-                    `Error removing folder: ${
-                        err1.message
-                    } and error removing data ${(err2 as Error).message}`
-                );
-            } else {
-                throw new Error(
-                    `Error removing data ${(err2 as Error).message}`
-                );
-            }
+    // fs.rm(project.path, { recursive: true }, async (err1) => {
+    //     try {
+    //         await removeProjectData(id);
+    //         if (err1) {
+    //             throw new Error(`Error removing folder ${err1.message}`);
+    //         }
+    //     } catch (err2) {
+    //         if (err1) {
+    //             throw new Error(
+    //                 `Error removing folder: ${
+    //                     err1.message
+    //                 } and error removing data ${(err2 as Error).message}`
+    //             );
+    //         } else {
+    //             throw new Error(
+    //                 `Error removing data ${(err2 as Error).message}`
+    //             );
+    //         }
+    //     }
+    // });
+
+    let removeFolderError: Error | null = null;
+
+    try {
+        await removeFolderAndContents(project.path);
+    } catch (err) {
+        removeFolderError = err as Error;
+    }
+
+    try {
+        await removeProjectData(id);
+    } catch (err) {
+        if (removeFolderError) {
+            throw new Error(
+                `Error removing folder: ${
+                    removeFolderError.message
+                } and error removing data ${(err as Error).message}`
+            );
+        } else {
+            throw new Error(`Error removing data ${(err as Error).message}`);
         }
-    });
+    }
+
+    if (removeFolderError) {
+        throw new Error(`Error removing folder ${removeFolderError.message}`);
+    }
 };
